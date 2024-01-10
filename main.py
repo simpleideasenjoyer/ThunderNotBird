34ceb7fd971bac60390d6ae412562eee7415f5d1
simpleideasenjoyer <e.soj.work@gmail.com>
2024-01-10 20:02:01 +0100

    update: connecting basic modules

--- main.py
+++ main.py
@@ -10,5 +10,18 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     login = LoginScreenWidget()
+    content = ContentWidget()
+    createMessage = CreateMessage()
+    mainWindow = MailBoxMainWindow()
+
+    login.show()
+    login.objectTransfer.connect(mainWindow.signal_receiver)
+    login.loginSuccessful.connect(mainWindow.show)
+
+    mainWindow.contentObject.connect(content.display_content)
+    mainWindow.openContent.connect(content.show)
+
+    mainWindow.openCreateMessage.connect(createMessage.show)
+    mainWindow.createMessageObject.connect(createMessage.get_message_data)
 
     sys.exit(app.exec_())
